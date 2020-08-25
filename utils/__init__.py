37e588c1295c8785323a08d2c5255506fc03fd29
henry_worklz@163.com <henry_worklz@163.com>
2020-08-25 21:29:46 +0800

    配置公共文件

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3.6.5
+# -*- coding: utf-8 -*-
+# author:henry time:2020/8/25 19:24
+
---
+++ utils/jinjia2_env.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+
+
+def jinjia2_env(**options):
+    """配置jinjia2环境变量"""
+    env = Environment(**options)
+    # 将static和url语法配置到变量中
+    env.globals.update({
+        "static": staticfiles_storage,
+        "url": reverse
+    })
+    return env
+
